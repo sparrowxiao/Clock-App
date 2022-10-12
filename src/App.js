be66de74cf4ec96e60518c5efdf6e7403122bc4e
Ting <workspacext@gmail.com>
2022-10-11 21:13:59 -0400

    DAY-1 Accomplishment
    1. Finished the basic UI layout.
    2. Imported font/images files to the React project, and applied with content.
    3. Use hooks to get the current time and change the page title.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import imoon from '../src/assets/desktop/icon-moon.svg';
 import './App.css';
+import { useState , useEffect } from 'react';
 
 function App() {
+  var [time,setTime]=useState(new Date());
+  useEffect(() => {
+    document.title = 'Clock App Challenage';
+    var timer = setInterval(()=>setTime(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Overlay">
+        <div className="Top">
+          <div className="Inspration">
+            <p className="Quote">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti."
+            </p>
+            <p className="Author">Ada LLL</p>
+          </div>
+        </div>
+        <div className="Bottom">
+          <div className="Current-time">
+            <div className="Greeting"><img src={imoon} className="Greeting-logo" alt="logo" /> Good Evening, it's currently</div>
+            <div className="Clock">{time.toLocaleTimeString('en-US', {hour: '2-digit',minute: '2-digit',hour12: false,})} <span className="Time-zone">EST</span></div>
+            <div className="Location">In Atlanta, USA</div>
+          </div>
+          <div className="More"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/fonts/Inter/static/Inter-Bold.ttf';
+import './assets/fonts/Inter/static/Inter-Light.ttf';
+import './assets/fonts/Inter/static/Inter-Regular.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
